0234316b5c2a3308c9bcfd0a8b611e3070b2dbb5
Bell_End <bnghty46@naver.com>
2025-09-05 15:59:15 +0900

    [commit] 나머지 연산자

---
+++ C#기본/코드 2-05/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(10 % 5);
+        Console.WriteLine(7 % 3);
+    }
+}
