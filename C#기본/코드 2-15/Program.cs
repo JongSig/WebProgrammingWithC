2b3ccfd307559df7a25717a33d1fdb9edb9ba352
Bell_End <bnghty46@naver.com>
2025-09-07 19:19:17 +0900

    [commit] 문자열 연결 연산자

---
+++ C#기본/코드 2-15/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
+    }
+}
