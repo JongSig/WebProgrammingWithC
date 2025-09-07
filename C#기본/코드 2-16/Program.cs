2bb90847ed990a99fd9ee34ab5f6c23f64412cd7
Bell_End <bnghty46@naver.com>
2025-09-07 19:34:26 +0900

    [commit] 문자 선택

---
+++ C#기본/코드 2-16/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("안녕하세요"[0]);
+        Console.WriteLine("안녕하세요"[1]);
+        Console.WriteLine("안녕하세요"[3]);
+    }
+}
