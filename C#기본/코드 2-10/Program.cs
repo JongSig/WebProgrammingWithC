13aa774a3ee6feb644fc8105098a36bf8aceeba9
Bell_End <bnghty46@naver.com>
2025-09-07 19:10:45 +0900

    [commit] 실수와 사칙 연산자

---
+++ C#기본/코드 2-10/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(1.0 + 2.0);
+        Console.WriteLine(1.0 - 2.0);
+        Console.WriteLine(1.0 * 2.0);
+        Console.WriteLine(1.0 / 2.0);
+    }
+}
