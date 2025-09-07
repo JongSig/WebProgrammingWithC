d8c62491c7f3aad168a4d93a3e441af5b98402c8
Bell_End <bnghty46@naver.com>
2025-09-07 19:04:40 +0900

    [commit] 음수와 나머지 연산자

---
+++ C#기본/코드 2-07/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(4 % 3);
+        Console.WriteLine(-4 % 3);
+        Console.WriteLine(4 % -3);
+        Console.WriteLine(-4 % -3);
+    }
+}
