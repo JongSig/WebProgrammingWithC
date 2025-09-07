250deaea295982cbfe9260d26035861d4c3560b0
Bell_End <bnghty46@naver.com>
2025-09-07 19:08:32 +0900

    [commit] 정수와 실수

---
+++ C#기본/코드 2-09/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(0); // 정수
+        Console.WriteLine(0.0); // 실수
+    }
+}
