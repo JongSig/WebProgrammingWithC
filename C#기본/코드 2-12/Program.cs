5185ca8bcc4f3414b039ba6283f94ef54cdf05b7
Bell_End <bnghty46@naver.com>
2025-09-07 19:12:50 +0900

    [commit] 문자

---
+++ C#기본/코드 2-12/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine('A');
+        Console.WriteLine('가');
+        Console.WriteLine('나');
+    }
+}
