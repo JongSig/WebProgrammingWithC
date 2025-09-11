d0500c8488e86e77203e65e3427c04c6793a14e6
Bell_End <bnghty46@naver.com>
2025-09-11 15:36:30 +0900

    [commit] 실습3

---
+++ 메소드/실습3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace AccessModifier1
+{
+    class AccessTest
+    {
+        public int num;
+
+        public AccessTest(int num)
+        {
+            this.num = num;
+        }
+        public void Show()
+        {
+            Console.WriteLine("num: " + num);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccessTest accesstest = new AccessTest(50);
+
+            accesstest.Show();
+        }
+    }
+}
