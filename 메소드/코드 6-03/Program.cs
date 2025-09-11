747cafed88f290e5eea026a9c1cd4cae9adbcddc
Bell_End <bnghty46@naver.com>
2025-09-11 14:37:22 +0900

    [commit] 아무것도 반환하지 않는 메서드

---
+++ 메소드/코드 6-03/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print() 메소드가 호출되었습니다.");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Test test = new Test();
+        test.Print();
+        test.Print();
+        test.Print();
+    }
+}
