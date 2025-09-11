2c78deb59accdd3933534fbe4c6f17d29f62da6a
Bell_End <bnghty46@naver.com>
2025-09-11 14:48:10 +0900

    [commit] 매개 변수와 변환(2)

---
+++ 메소드/기본예제 6-03/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Test
+    {
+        public int Multiply(int min, int max)
+        {
+            int output = 1;
+            for (int i = min; i <= max; i++)
+            {
+                output *= i;
+            }
+            return output;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Test test = new Test();
+        Console.WriteLine(test.Multiply(1, 10));
+    }
+}
