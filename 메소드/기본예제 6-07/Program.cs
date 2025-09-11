13baf7576223cbe6dd315b320a0ebb9a6aea844f
Bell_End <bnghty46@naver.com>
2025-09-11 15:40:20 +0900

    [commit] 소멸자 생성과 사용

---
+++ 메소드/기본예제 6-07/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Product
+    {
+        public string name;
+        public int price;
+
+        public Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        ~Product()
+        {
+            Console.WriteLine(this.name + "의 소멸자 호출");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Product product = new Product("냉장고", 100);
+    }
+}
