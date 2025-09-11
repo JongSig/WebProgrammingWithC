47167d999fe9acada356a2a10013923bf033b5a3
Bell_End <bnghty46@naver.com>
2025-09-11 15:17:20 +0900

    [commit] 메서드 오버로딩

---
+++ 메소드/기본예제 6-05/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class MyMath
+    {
+        public static int Abs(int input)
+        {
+            if (input < 0) { return -input; }
+            else { return input; }
+        }
+        public static double Abs(double input)
+        {
+            if (input < 0) { return -input; }
+            else { return input; }
+        }
+        public static double Abs(long input)
+        {
+            if (input < 0) { return -input; }
+            else { return input; }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MyMath.Abs(52));
+        Console.WriteLine(MyMath.Abs(-273));
+        Console.WriteLine(MyMath.Abs(52.273));
+        Console.WriteLine(MyMath.Abs(-32.103));
+        Console.WriteLine(MyMath.Abs(52273123456789));
+        Console.WriteLine(MyMath.Abs(-52273123456789));
+    }
+}
