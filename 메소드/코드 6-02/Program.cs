39f17485cb607d40084026ba8cc7b850f9eed5e1
Bell_End <bnghty46@naver.com>
2025-09-11 14:34:47 +0900

    [commit] 두 개의 매개변수를 갖는 메서드

---
+++ 메소드/코드 6-02/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Test
+    {
+        public int Multi(int x, int y)
+        {
+            return x * y;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Test test = new Test();
+        Console.WriteLine(test.Multi(52, 273));
+        Console.WriteLine(test.Multi(103, 32));
+    }
+}
