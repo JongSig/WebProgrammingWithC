5e9af188ef98bccd52046a8834a976574ae229bd
Bell_End <bnghty46@naver.com>
2025-09-11 14:31:15 +0900

    [commit] 인스턴스 메서드 생성과 사용

---
+++ 메소드/코드 6-01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Test
+    {
+        public int Power(int x)
+        {
+            return x * x;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Test test = new Test();
+        Console.WriteLine(test.Power(10));
+        Console.WriteLine(test.Power(20));
+    }
+}
+
