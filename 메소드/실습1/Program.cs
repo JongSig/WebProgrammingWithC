2a3a0bdb37c9f3ad5511af55bc506d5e8a07afd8
Bell_End <bnghty46@naver.com>
2025-09-11 14:51:01 +0900

    [commit] 실습1

---
+++ 메소드/실습1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Test
+    {
+        public long Multiply(int min, int max)
+        {
+            long output = 1;
+            for (int i = min; i <= max; i++)
+            {
+                output *= i;
+            }
+            return output;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Test test = new Test();
+        Console.WriteLine(test.Multiply(1, 33));
+    }
+}
