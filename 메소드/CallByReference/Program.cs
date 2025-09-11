65e1df982494c96053058d82ebff970ffd383c9e
Bell_End <bnghty46@naver.com>
2025-09-11 14:58:27 +0900

    [commit] Call by reference(3)

---
+++ 메소드/CallByReference/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Swap(ref int a, ref int b)
+    {
+        int temp = b;
+        b = a;
+        a = temp;
+        Console.WriteLine("Swap : a = {0}, b = {1}, b = {2}", a, b, temp);
+    }
+
+    static void Main(string[] args)
+    {
+        int a = 40;
+        int b = 10;
+
+        Console.WriteLine("Swap before : a = {0}, b = {1}", a, b);
+
+        Swap(ref a, ref b);
+
+        Console.WriteLine("Swap after : a = {0}, b = {1}", a, b);
+    }
+}
