733722e765da04adae88aabd3a8949f7dfbc4031
Bell_End <bnghty46@naver.com>
2025-09-11 14:42:07 +0900

    [commit] 매개 변수와 변환(1)

---
+++ 메소드/기본예제 6-02/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class Test 
+    {
+        public int Sum(int min, int max)
+        {
+            int output = 0;
+            for (int i = min; i <= max; i++)
+            {
+                output += i;
+            }
+            return output;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Test test = new Test();
+        Console.WriteLine(test.Sum(1, 100));
+    }
+}
