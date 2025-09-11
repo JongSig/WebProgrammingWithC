fbf3ac8a6841121fbeec5c4ba74d7e2692833afa
Bell_End <bnghty46@naver.com>
2025-09-11 15:05:18 +0900

    [commit] 클래스 메서드 생성과 사용

---
+++ 메소드/기본예제 6-04/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    class MyMath
+    {
+        public static int Abs(int input)
+        {
+            if(input < 0)
+            {
+                return -input;
+            }
+            else
+            {
+                return input;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MyMath.Abs(52));
+        Console.WriteLine(MyMath.Abs(-273));
+    }
+}
